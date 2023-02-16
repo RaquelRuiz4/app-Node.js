e2f11a84977a488d5eec79af24aa2dd8b9624f09
RaquelR <raquel.ruiz.alu@iescampanillas.com>
2023-02-16 20:08:15 +0100

    Add files via upload

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/bash -xe
+
 
 # Start Install
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
@@ -24,29 +25,3 @@ ln -sf "$NVM_DIR/versions/node/$(nvm version)/bin/npm" "/usr/local/bin/npm"
 
 
 
-
-
-
-
-
-# Instalar MVN
-#           cat > /tmp/install_script.sh << EOF
-#             # Start Install
-#             echo "Instalando dependencias Node.JS"
-#             curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-#             . ~/.nvm/nvm.sh
-#             . ~/.bashrc
-#             node -v
-#             npm -v
-#             nvm install --lts
-#             curl -sL https://github.com/RaquelRuiz4/app-Node.js/archive/master.zip --output master.zip
-#             mkdir -p ~/logs
-#             unzip master.zip
-#             mv aws-helloworld-node-master/ app 
-#             cd app
-#             npm install
-#             npm start
-
-       
-#           chown ubuntu:ubuntu /tmp/install_script.sh && chmod a+x /tmp/install_script.sh
-#            sleep 1; su - ubuntu -c "/tmp/install_script.sh"
