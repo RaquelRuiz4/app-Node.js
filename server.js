b79d3bf40fd8c293bbd6aa9481f11a8ebe9fe56b
RaquelR <raquel.ruiz.alu@iescampanillas.com>
2023-02-23 09:22:36 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-const { hostname } = require('os');
-const http = require('http');
+const { hostname } = require("os");
+const http = require("http");
 
 const message = "Hola Mundo";
 const port = 8080;
 
-const server = http.createServer((req,res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type',"text/plain");
-    res.end(message);
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end(message);
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Servidor corriendo en http://${hostname()}:${port}/`);
+  console.log(`Servidor corriendo en http://${hostname()}:${port}/`);
 });
-
