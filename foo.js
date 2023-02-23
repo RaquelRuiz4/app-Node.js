0b03812cd910794dc672dda06bada0f6bbaf97fa
RaquelR <raquel.ruiz.alu@iescampanillas.com>
2023-02-23 09:20:08 +0100

    Add files via upload

---
+++ foo.js
@@ -0,0 +1,4 @@
+function foo(a) {
+  return a;
+}
+module.exports = foo;
---
+++ foo.test.js
@@ -0,0 +1,10 @@
+const foo = require('./foo.test');
+
+test("foobar test", () => {
+// arrange and act
+let expected = 10;
+let result = foo(10);
+
+// assert
+expect(result).toBe(10);
+});
